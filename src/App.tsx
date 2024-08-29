4bd7323dc1ded03c8403e883425d32e7e917414a
Gustavo <gustavohenrique@github.com>
2024-08-29 12:15:55 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages
+import FormLogin from "./pages/formlogin";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<FormLogin/>} />
+        {/* <Route path="/criarconta" element={<Form/>} /> */}
+        <Route path="/" element={<Header/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
