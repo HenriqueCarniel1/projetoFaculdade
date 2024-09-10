de034a98e84e891116a447cfefed558768cded3c
Gabriel Munnier Nogueira Silva <gabrielmunnier28@dinfor.uniceub>
2024-09-10 10:37:40 -0300

    formulario de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FormLogin from "./pages/formlogin";
 import Home from "./pages/Home";
 import Carrinho from "./pages/carrinho";
+import FormCreate from "./pages/formcreate";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<FormLogin/>} />
+        <Route path="/formcreatelogin" element={<FormCreate/>} />
         <Route path="/" element={<Home/>} />
         <Route path="/carrinho" element={<Carrinho/>} />
       </Routes>
--- src/pages/formcreate/index.tsx
+++ src/pages/formcreate/index.tsx
@@ -1,12 +1,84 @@
-import './style.css';
+import axios from "axios";
+import React, { useState, FormEvent } from "react";
+import "./style.css";
+
 
 function FormCreate() {
-    return(
-        // Gabriel
-        <div>
 
+    return (
+        <div id="container-login">
+            <div id="container-form">
+                <div id="titulo-div-input">
+                    <h1>Cadrastro</h1>
+                </div>
+                <form className="formCreate">
+                    <div id="nome-div-input">
+                        <label>Nome:</label>
+                        <input
+                            type="text"
+                            name="Nome"
+                            placeholder="Digite seu nome"
+
+                        />
+                        <p id="emailverificated" style={{ color: 'red' }}>
+                        </p>
+                    </div>
+                    <div id="email-div-input">
+                        <label>Email:</label>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Digite um email"
+
+                        />
+                        <p id="emailverificated" style={{ color: 'red' }}>
+                        </p>
+                    </div>
+                    <div id="criar-senha-div-input">
+                        <label>Criar senha:</label>
+                        <input
+                            type="text"
+                            name="Criar senha"
+                            placeholder="Crie sua senha"
+
+                        />
+                        <p id="criar-senhaverificated" style={{ color: 'red' }}>
+                        </p>
+                    </div>
+                    <div id="confirmar-senha-div-input">
+                        <label>Confirme sua senha:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Confirme sua senha"
+
+                        />
+                        <p id="onfirmar-senha-verificated" style={{ color: 'red' }}>
+                        </p>
+                    </div>
+                    <div id="telefone-div-input">
+                        <label>Telefone:</label>
+                        <input
+                            type="text"
+                            name="telefone"
+                            placeholder="Digite seu número de telefone"
+
+                        />
+                        <p id="telefoneverificated" style={{ color: 'red' }}>
+                        </p>
+                    </div>
+                    <div id="mensagem-certo">
+                    </div>
+
+                    <div id="botao-div">
+                        <button type="submit" id="botao">Criar conta</button>
+
+
+                    </div>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
 
 export default FormCreate;
